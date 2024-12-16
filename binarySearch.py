5f23969b17c81a82188afc79cd20e2a160907e33
cassiezhs <zs195@georgetown.edu>
2024-12-15 19:05:49 -0500

    add new questions

--- binarySearch.py
+++ binarySearch.py
@@ -1,4 +1,19 @@
 '''Binary search O(log2n)'''
+def binary_search(sequence, item):
+    begin_point = 0
+    end_point = len(sequence) - 1
+    while begin_point <= end_point:
+        midpoint = begin_point + (begin_point + end_point) // 2
+        midpoint_value = sequence[midpoint]
+        if item == midpoint_value:
+            return midpoint
+        elif item < midpoint:
+            end_index = midpoint - 1
+        else:
+            start_point = midpoint + 1
+    return None
+
+'''35. Search Insertion Position'''
 def searchInsert(nums, target):
         """
         :type nums: List[int]
@@ -18,3 +33,6 @@ def searchInsert(nums, target):
                 start_point = midpoint + 1
         # If the target is not found, start_point will be the insert position
         return start_point
+nums_a = [1,3,5,6]
+target_a = 5
+print(searchInsert(nums_a, target_a))
