04d9344ec9d2b683e3bde1458ab49d54edb5f0ee
cassiezhs <zs195@georgetown.edu>
2025-07-08 22:02:53 -0400

    added new question

--- Array.py
+++ Array.py
@@ -42,4 +42,22 @@ print(nums)
 
 
                     
-                        
+'''
+136. single number
+'''                        
+def singleNumber(nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        no_duplicate_list = []
+        for i in nums:
+                if i not in no_duplicate_list:
+                        no_duplicate_list.append(i)
+                else:
+                      no_duplicate_list.remove(i)
+        return no_duplicate_list.pop()
+nums = [2,2,1]
+print(singleNumber(nums))
+                      
+             
